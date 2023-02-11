0bd22f1a3bd2f8ae41dd11e303203044a22afaad
X-y-l <georgetownsend24@yahoo.com>
2023-02-11 22:28:19 +0000

    adding fish

---
+++ PythonGame/Fishing_4_complements.py
@@ -0,0 +1,28 @@
+complements = [
+    "You look terribly great today!",
+    "I love how your eyes look!",
+    "Your hair is beautiful.",
+    "I like your style."
+    "You should be proud of yourself!",
+    "You're a perfect 10!",
+    "The world is better with you in it."
+    "Your voice is angelic!",
+    "I love being around you.",
+    "I almost passed out when I saw you, you're stunning!",
+    "You're so intelligent and creative.",
+    "Hanging out with you is always a blast."
+]
+
+insults = [
+    "Eww, you stink!",
+    "Are you an angel? Because you look like you fell from heaven.",
+    "Wow, you're at the top of the bell curve!",
+    "You're the most beautiful 3 I have ever met.",
+    "I can't wait for you to leave. Please get out.",
+    "You are very lame.",
+    "You might be the least cool person in the universe.",
+    "You are simply grotesque.",
+    "You look like a naked mole rat.",
+    "When I saw you I threw up in my mouth a little.",
+    "You look great! I couldn't even recognize you!",
+]
